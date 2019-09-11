5d6f55f95745fe77df3ad1bac85a9f244b5f5b02
Beto <37488743+betowxyz@users.noreply.github.com>
2019-09-11 17:01:43 -0300

    Create visibilidade.h
    Header do modulo de Visibilidade.

---
+++ visibilidade.h
@@ -0,0 +1,8 @@
+/*
+
+    Declaracao das funcoes do modulo VISIBILIDADE (1)
+
+*/
+
+int processamentoVisibilidade(Mapa *mapa, Visibilidade *visibilidade); // Calcula os pontos necessarios para visao total do mapa
+void raio(int xa, int ya, int xb, int yb, Mapa *mapa); // Cria raio verificando a visibilidade do ponto X ao ponto Y
