54e3c96e692b61c3b3d5122755ac1509c851c6af
Beto <37488743+betowxyz@users.noreply.github.com>
2019-09-25 04:16:06 -0300

    Delete pathPlanning.h

--- interface/pathPlanning.h
+++
@@ -1,8 +0,0 @@
-/*
-
-    Declaracao das funcoes do modulo PATH FINDING (2)
-
-*/
-
-float heuristica(Ponto a, Ponto b); // Calculo da heuristica de Manhattan
-Path * aStar(Ponto inicio, Ponto objetivo, Mapa *mapa); // Algoritmo de Path Finding
