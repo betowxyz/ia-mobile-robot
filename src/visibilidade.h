3d6a65e79579018222c0a9947c7bb667305e1cc6
Beto <37488743+betowxyz@users.noreply.github.com>
2019-09-25 04:17:43 -0300

    Delete visibilidade.h

--- src/visibilidade.h
+++
@@ -1,8 +0,0 @@
-/*
-
-    Declaracao das funcoes do modulo VISIBILIDADE (1)
-
-*/
-
-int processamentoVisibilidade(Mapa *mapa, Visibilidade *visibilidade); // Calcula os pontos necessarios para visao total do mapa
-void raio(int xa, int ya, int xb, int yb, Mapa *mapa); // Cria raio verificando a visibilidade do ponto X ao ponto Y
