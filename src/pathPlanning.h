d0a8d81801a7440b09065d5ebdd357b2382b4f05
Beto <37488743+betowxyz@users.noreply.github.com>
2019-09-25 04:17:22 -0300

    Delete pathPlanning.h

--- src/pathPlanning.h
+++
@@ -1,9 +0,0 @@
-/*
-
-    Declaracao das funcoes do modulo PATH FINDING (2)
-
-*/
-
-void ordenaGuardas(Visibilidade *visibilidade); // Ordena a lista de pontos de guarda de acordo com a heuristica
-float heuristica(Ponto a, Ponto b); // Calculo da heuristica de Manhattan
-Path * aStar(Ponto inicio, Ponto objetivo, Mapa *mapa); // Algoritmo de Path Finding
